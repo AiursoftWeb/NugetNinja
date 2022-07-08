6bd686351b9d91bfec31701114638588d4d23556
Anduin Xue <xuef@microsoft.com>
2022-07-08 15:59:35 +0800

    Enumerate all csprojs.

---
+++ src/Entry.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aiursoft.NugetNinja
+{
+    public class Entry
+    {
+        private readonly ILogger<Entry> logger;
+
+        public Entry(ILogger<Entry> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task StartEntry(string[] args)
+        {
+            logger.LogInformation("Starting NugetNinja...");
+
+            if (args.Length < 1)
+            {
+                logger.LogWarning("Usage: [Working path]");
+                return;
+            }
+
+            var workingPath = args[0];
+            var csprojs = Directory
+                .EnumerateFiles(workingPath, "*.csproj", SearchOption.AllDirectories)
+                .ToArray();
+
+            foreach (var csproj in csprojs)
+            {
+                logger.LogTrace($"Parsing {csproj}...");
+            }
+
+            await Task.Delay(1000);
+
+            logger.LogInformation("Stopping NugetNinja...");
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Aiursoft.NugetNinja;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        services.AddLogging(logging =>
+        {
+            logging.AddConsole();
+            logging.SetMinimumLevel(LogLevel.Trace);
+        });
+
+        services.AddSingleton<Entry>();
+
+        var serviceProvider = services.BuildServiceProvider();
+        var entry = serviceProvider.GetRequiredService<Entry>();
+        await entry.StartEntry(new string[] { @"C:\Users\xuef\source\repos\AiursoftWeb" });
+    }
+}
