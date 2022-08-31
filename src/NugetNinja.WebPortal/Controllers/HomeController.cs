7b376eae220826fad62e2b8f0a56605eb4fd3f54
Anduin Xue <Anduin@aiursoft.com>
2022-08-31 23:27:28 +0800

    Fill in basic actions.

--- src/NugetNinja.WebPortal/Controllers/HomeController.cs
+++ src/NugetNinja.WebPortal/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Aiursoft.NugetNinja.WebPortal.Controllers;
 
@@ -11,9 +12,49 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    [HttpGet]
     public IActionResult Index()
     {
         _logger.LogInformation("A user accessed the index page.");
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Subscribe([FromForm][Required][Url]string githubUrl)
+    {
+        _logger.LogInformation($"A user subscribed the repo: {githubUrl}");
+
+        // Really subscribe.
+        return RedirectToAction(nameof(SuccessfullySubscribed));
+    }
+
+    [HttpGet]
+    public IActionResult SuccessfullySubscribed()
+    {
+        throw new NotImplementedException();
+        // return view();
+    }
+
+    [HttpGet]
+    public IActionResult ConsiderUnsubscribe([FromQuery]string? repo)
+    {
+        throw new NotImplementedException();
+        // return view();
+    }
+
+    [HttpPost]
+    public IActionResult Unsubscribe([FromForm][Required][Url]string githubUrl)
+    {
+        _logger.LogInformation($"A user unsubscribed the repo: {githubUrl}");
+
+        // Really unsubscribe.
+        return RedirectToAction(nameof(SuccessfullyUnSubscribed));
+    }
+
+    [HttpGet]
+    public IActionResult SuccessfullyUnSubscribed()
+    {
+        throw new NotImplementedException();
+        // return view();
     }
 }
