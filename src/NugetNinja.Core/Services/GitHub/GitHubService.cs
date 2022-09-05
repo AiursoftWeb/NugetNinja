1122193ac523d2b5e6fe37dd7bf10767617b06c2
Edi Wang <edi.wang@outlook.com>
2022-09-05 14:52:57 +0800

    remove fucked namespace

--- src/NugetNinja.Core/Services/GitHub/GitHubService.cs
+++ src/NugetNinja.Core/Services/GitHub/GitHubService.cs
@@ -3,7 +3,6 @@
 using System.Net;
 using System.Text;
 using System.Text.Json;
-using Aiursoft.NugetNinja.Core.Services.Utils;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
