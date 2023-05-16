359016c8a12230f5d50f93574347c3207c95a717
Anduin Xue <anduin@aiursoft.com>
2023-05-16 10:14:38 +0000

    Fix lint issue.

--- src/NugetNinja.Core/Model/Framework/AppSettings.cs
+++ src/NugetNinja.Core/Model/Framework/AppSettings.cs
@@ -5,6 +5,6 @@ public class AppSettings
     public bool AllowCross { get; set; }
     public bool Verbose { get; set; }
     public bool AllowPreview { get; set; }
-    public string CustomNugetServer { get; set; }
-    public string PatToken { get; set; }
+    public string CustomNugetServer { get; set; } = string.Empty;
+    public string PatToken { get; set; } = string.Empty;
 }
