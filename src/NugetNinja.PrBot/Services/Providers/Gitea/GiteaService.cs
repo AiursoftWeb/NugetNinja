de34f1c3af1b61eb1163e4ba88a5d177b2a5402c
Anduin Xue <anduin@aiursoft.com>
2023-05-17 18:10:50 +0000

    Use unified gitea endpoint grammar. Clean token.

--- src/NugetNinja.PrBot/Services/Providers/Gitea/GiteaService.cs
+++ src/NugetNinja.PrBot/Services/Providers/Gitea/GiteaService.cs
@@ -25,7 +25,7 @@ public class GiteaService : IVersionControlService
         _logger.LogInformation($"Getting if repository exists based on org: {orgName}, repo: {repoName}...");
         try
         {
-            var endpoint = $@"{endPoint}/repos/{orgName}/{repoName}";
+            var endpoint = $@"{endPoint}/api/v1/repos/{orgName}/{repoName}";
             await _httpClient.SendHttp(endpoint, HttpMethod.Get, patToken);
             return true;
         }
@@ -40,7 +40,7 @@ public class GiteaService : IVersionControlService
         _logger.LogInformation($"Listing all stared repositories based on user's name: {userName}...");
         for (var i = 1;; i++)
         {
-            var endpoint = $@"{endPoint}/users/{userName}/starred?page={i}";
+            var endpoint = $@"{endPoint}/api/v1/users/{userName}/starred?page={i}";
             var currentPageItems = await _httpClient.SendHttpAndGetJson<List<Repository>>(endpoint, HttpMethod.Get, patToken);
             if (!currentPageItems.Any()) yield break;
 
@@ -52,7 +52,7 @@ public class GiteaService : IVersionControlService
     {
         _logger.LogInformation($"Forking repository on Gitea with org: {org}, repo: {repo}...");
 
-        var endpoint = $@"{endPoint}/repos/{org}/{repo}/forks";
+        var endpoint = $@"{endPoint}/api/v1/repos/{org}/{repo}/forks";
         await _httpClient.SendHttp(endpoint, HttpMethod.Post, patToken);
     }
 
@@ -61,7 +61,7 @@ public class GiteaService : IVersionControlService
     {
         _logger.LogInformation($"Getting pull requests on Gitea with org: {org}, repo: {repo}...");
 
-        var endpoint = $@"{endPoint}/repos/{org}/{repo}/pulls?head={head}";
+        var endpoint = $@"{endPoint}/api/v1/repos/{org}/{repo}/pulls?head={head}";
         return await _httpClient.SendHttpAndGetJson<List<PullRequest>>(endpoint, HttpMethod.Get, patToken);
     }
 
@@ -70,7 +70,7 @@ public class GiteaService : IVersionControlService
     {
         _logger.LogInformation($"Creating a new pull request on Gitea with org: {org}, repo: {repo}...");
 
-        var endpoint = $@"{endPoint}/repos/{org}/{repo}/pulls";
+        var endpoint = $@"{endPoint}/api/v1/repos/{org}/{repo}/pulls";
         await _httpClient.SendHttp(endpoint, HttpMethod.Post, patToken, new
         {
             title = "Auto dependencies upgrade by bot.",
