e1361bef891549cf63dadd9dfbba589a46068291
Anduin Xue <anduin@aiursoft.com>
2023-08-20 17:24:47 +0000

    Project with program treat as executable.

--- src/Aiursoft.NugetNinja.MissingPropertyPlugin/Services/ProjectTypeDetector.cs
+++ src/Aiursoft.NugetNinja.MissingPropertyPlugin/Services/ProjectTypeDetector.cs
@@ -15,6 +15,8 @@ public class ProjectTypeDetector
     
     public ProjectInfo Detect(Project project)
     {
+        var hasProgram = CheckForProgramCs(project.PathOnDisk);
+        
         var isWebProject = project.Sdk?.ToLower().EndsWith("web") ?? false;
         _logger.LogTrace("Project {Project} is web project status is {UseWeb}", project, isWebProject);
 
@@ -30,14 +32,24 @@ public class ProjectTypeDetector
         var hasVersion = !string.IsNullOrWhiteSpace(project.Version);
         _logger.LogTrace("Project {Project} built has version status is {HasVersion}", project, hasVersion);
 
+        var isExecutable = hasProgram || packAsTool || isWebProject || (project.OutputType?.ToLower().EndsWith("exe") ?? false);
         return new ProjectInfo
         {
-            IsExecutable = hasWindows || packAsTool || isWebProject || (project.OutputType?.EndsWith("exe") ?? false),
-            IsWindowsExecutable = hasWindows,
+            IsExecutable = isExecutable,
+            IsWindowsExecutable = hasWindows && isExecutable,
             IsHttpServer = isWebProject,
             IsUnitTest = hasUtFeatures,
             ShouldPackAsNugetTool = !hasUtFeatures && (hasVersion && packAsTool),
             ShouldPackAsNugetLibrary = !hasUtFeatures && (hasVersion || packAsTool)
         };
+    }
+    
+    private bool CheckForProgramCs(string filePath)
+    {
+        var directoryPath = Path.GetDirectoryName(filePath);
+        if (directoryPath == null) return false;
+        var files = Directory.GetFiles(directoryPath, "Program.cs");
+        return files.Length > 0;
+
     }
 }
