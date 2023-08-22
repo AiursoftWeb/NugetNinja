527d6a0b238bcd187e5c4fd6d842a11c82977864
Anduin Xue <anduin@aiursoft.com>
2023-08-22 16:32:06 +0000

    Working visualizer plugin with depth.

--- src/Aiursoft.NugetNinja.VisualizerPlugin/Entry.cs
+++ src/Aiursoft.NugetNinja.VisualizerPlugin/Entry.cs
@@ -2,18 +2,57 @@ using Aiursoft.NugetNinja.Core;
 
 namespace Aiursoft.NugetNinja.VisualizerPlugin;
 
-public class Entry : IEntryService
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Entry
 {
+    private readonly NugetService _nugetService;
     private readonly Extractor _extractor;
 
-    public Entry(Extractor extractor)
+    public Entry(
+        NugetService nugetService,
+        Extractor extractor)
     {
+        _nugetService = nugetService;
         _extractor = extractor;
     }
-    
-    public async Task OnServiceStartedAsync(string path, bool shouldTakeAction)
+
+    public async Task OnServiceStartedAsync(string path, int depth)
     {
         var model = await _extractor.Parse(path);
-        Console.WriteLine(model.AllProjects.Count);
+        await foreach (var result in GenerateRelationships(model, depth).Distinct())
+        {
+            Console.WriteLine(result);
+        }
+    }
+
+    private async IAsyncEnumerable<string> GenerateRelationships(Model model, int depth)
+    {
+        foreach (var project in model.AllProjects)
+        {
+            foreach (var pr in project.ProjectReferences)
+            {
+                yield return $"    {project.FileName} --> {pr.FileName}";
+            }
+
+            foreach (var pr in project.PackageReferences)
+            {
+                yield return $"    {project.FileName} --> {pr.Name}";
+                await foreach (var generated in WritePackageInfo(pr, currentDepth: 1, maxDepth: depth)) yield return generated;
+            }
+        }
+    }
+
+    private async IAsyncEnumerable<string> WritePackageInfo(Package package, int currentDepth = 1, int maxDepth = 2)
+    {
+        if (currentDepth >= maxDepth)
+        {
+            yield break;
+        }
+        var refs = await _nugetService.GetPackageDependencies(package);
+        foreach (var reference in refs ?? Array.Empty<Package>())
+        {
+            yield return $"    {package.Name} --> {reference.Name}";
+            await foreach (var generated in WritePackageInfo(reference, currentDepth + 1, maxDepth)) yield return generated;
+        }
     }
 }
--- src/Aiursoft.NugetNinja.VisualizerPlugin/VisualizerPlugin.cs
+++ src/Aiursoft.NugetNinja.VisualizerPlugin/VisualizerPlugin.cs
@@ -1,6 +1,11 @@
+using System.CommandLine;
+using Aiursoft.Canon;
 using Aiursoft.CommandFramework.Abstracts;
 using Aiursoft.CommandFramework.Framework;
+using Aiursoft.CommandFramework.Services;
 using Aiursoft.NugetNinja.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Aiursoft.NugetNinja.VisualizerPlugin;
 
@@ -12,9 +17,74 @@ public class VisualizerPlugin : IPlugin
     }
 }
 
-public class VisualizerHandler : ServiceCommandHandler<Entry, StartUp>
+public sealed class VisualizerHandler : CommandHandler
 {
     public override string Name => "visualize";
 
     public override string Description => "The command to visualize the dependency relationship, with mermaid markdown.";
+    
+    public static readonly Option<int> DepthOption = new(
+        new[] { "--depth", "-d" },
+        "Depth for package reference");
+
+    public override Option[] GetCommandOptions()
+    {
+        return new Option[] { DepthOption };
+    }
+
+    public override void OnCommandBuilt(Command command)
+    {
+        command.SetHandler(
+            Execute,
+            OptionsProvider.PathOptions,
+            DepthOption,
+            OptionsProvider.VerboseOption,
+            OptionsProvider.CustomNugetServer,
+            OptionsProvider.PatToken);
+    }
+
+    private Task Execute(
+        string path, 
+        int depth,
+        bool verbose,
+        string customNugetServer,
+        string patToken)
+    {
+        var services = BuildServices(verbose, customNugetServer, patToken);
+        return RunFromServices(services, path, depth);
+    }
+
+    private ServiceCollection BuildServices(
+        bool verbose, 
+        string customNugetServer,
+        string patToken)
+    {
+        var services = ServiceBuilder.BuildServices<StartUp>(verbose);
+        services.AddMemoryCache();
+        services.AddHttpClient();
+        services.AddTaskCanon();
+        services.AddTransient<Extractor>();
+        services.AddTransient<ProjectsEnumerator>();
+        services.AddTransient<NugetService>();
+        services.AddTransient<VersionCrossChecker>();
+        services.Configure<AppSettings>(options =>
+        {
+            options.Verbose = verbose;
+            options.CustomNugetServer = customNugetServer;
+            options.PatToken = patToken;
+        });
+        services.AddTransient<Entry>();
+        return services;
+    }
+
+    private Task RunFromServices(ServiceCollection services, string path, int depth)
+    {
+        var serviceProvider = services.BuildServiceProvider();
+        var service = serviceProvider.GetRequiredService<Entry>();
+        var logger = serviceProvider.GetRequiredService<ILogger<Entry>>();
+
+        var fullPath = Path.GetFullPath(path);
+        logger.LogTrace(@"Starting service: '{Name}'. Full path is: '{FullPath}'", nameof(Entry), fullPath);
+        return service.OnServiceStartedAsync(fullPath, depth);
+    }
 }
